0d11fb8e339ef6eb972d2feb83d1c54608b6c103
Your Name <batool.samoudi@gmail.com>
2021-04-27 21:37:59 +0300

    version 1.0

---
+++ app/src/main/java/edu/birzeit/assignment2/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.birzeit.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void nextPageBtn(View view){
+
+    }
+}
---
+++ app/src/main/java/edu/birzeit/assignment2/model/CV.java
@@ -0,0 +1,5 @@
+package edu.birzeit.assignment2.model;
+
+public class CV {
+
+}
